9a06d04dfdd5d112ce8b5dac730457025f5cf082
SimonBard <github@regenpost.de>
2024-04-30 12:15:39 +0200

    readme

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 import lib.manager
 
 
+def setup()-> None:
+    
+    dbmanager = lib.manager.Manager()
+    dbmanager.create_tables()
+    
 def main():
     setup()
 
 if __name__ == "__main__":
     main()
-
-def setup()-> None:
-    
-    dbmanager = lib.manager.Manager()
-    dbmanager.create_tables()
